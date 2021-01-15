d03a7f1ac794be8b0cbfff803e2900a6d8d7e04a
favachinho <carlos.favacho@vibedesenv.com>
2021-01-15 00:14:31 -0300

    create backup vm

--- storage/modules/recoveryservices/vault.js
+++ storage/modules/recoveryservices/vault.js
@@ -2,9 +2,38 @@ const azure = require("@pulumi/azure");
 const azure_nextgen = require("@pulumi/azure-nextgen");
 const {createDiagSetting} = require('../../../root/modules/insights/diagnostic');
 
+async function createBackupVm(rsv,rg,bkPpolicy,vmId,vmName, azureProvider) {
+    const st = bkPpolicy.toString().replace('Subscriptions','subscriptions');
+    const protectedItem = await new azure.backup.ProtectedVM(`protect-${vmName}`,{
+        backupPolicyId : st,
+        resourceGroupName: rg.name,
+        recoveryVaultName: rsv,
+        sourceVmId: vmId,
+        tags : {
+            name: `protect-${vmName}`,
+            environment: 'mgmt'
+        }
+    },{provider: azureProvider});
+
+    //Bug in create autorest/azure: error response cannot be parsed: "" error: EOF
+    // const protectedItem = await new azure_nextgen.recoveryservices.latest.ProtectedItem(`protect-${vmName}`, {
+    //     containerName: `IaasVMContainer;iaasvmcontainerv2;${rg.name};${vmName}`,
+    //     fabricName: "Azure",
+    //     properties: {
+    //         policyId: bkPpolicy.id,
+    //         protectedItemType: "Microsoft.Compute/virtualMachines",
+    //         sourceResourceId: vmId,
+    //     },
+    //     protectedItemName: `VM;iaasvmcontainerv2;${rg.name};${vmName}`,
+    //     resourceGroupName: rg.name,
+    //     vaultName: rsv,
+    // },{provider: azureProvider});
+
+    return protectedItem;
+}
 
 async function createDefaultPolicyBkp(rsv,namePolicy,type,wkType,rg,azureProvider) {
-    const protectionPolicy = new azure_nextgen.recoveryservices.latest.ProtectionPolicy(namePolicy, {
+    const protectionPolicy = await new azure_nextgen.recoveryservices.latest.ProtectionPolicy(namePolicy, {
         policyName: namePolicy,
         properties: {
             backupManagementType: type,
@@ -59,5 +88,5 @@ async function createRSV(name,rg,azureProviderOld,azureProviderNew,diag,diagDef)
 }
 
 module.exports = {
-    createRSV,createDefaultPolicyBkp
+    createRSV,createDefaultPolicyBkp,createBackupVm
 }
